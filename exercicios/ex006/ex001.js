6cf7477cb5c8abcf95b4f2cb9cbf3305e43b8768
matheus <matheuz1398@gmail.com>
2024-08-31 22:54:50 -0300

    .

--- exercicios/ex006/ex001.js
+++ exercicios/ex006/ex001.js
@@ -1,2 +1,10 @@
-console.log('hello world')
-window.AbortController
+var pais = "eua"
+console.log(`vivendo no ${pais}`)
+
+if(pais == "eua"){
+    console.log('voce e americano!')
+}
+if(pais == "br" ){
+    console.log('voce e brasileiro!')
+}
+
