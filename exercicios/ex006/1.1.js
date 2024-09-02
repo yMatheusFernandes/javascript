7f2d385355d4772ddce2f96794cb7506f79d43e5
matheus <matheuz1398@gmail.com>
2024-09-01 22:40:49 -0300

    .

---
+++ exercicios/ex006/1.1.js
@@ -0,0 +1,14 @@
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+
+console.log(`${a}`, `${b}`, `${c}`)
+
+console.log(d,a,b,c)
+
+if('a'>'b'){
+    console.log('A e maior que B!')
+}else{
+    console.log('C não e maior que B')
+}
---
+++ exercicios/ex006/2.2.js
@@ -0,0 +1,14 @@
+var idade = 2112
+
+if(idade > 17 ){
+    console.log('voto Obrigatorio')
+}else{
+    if(idade >= 16){
+        console.log('Voto opcional')
+    }else{
+        if(idade < 16){
+            console.log('Não pode votar')
+        
+        }
+    }
+}
