74447f947ccad091d401f8d28f05aa19174432a5
matheus <matheuz1398@gmail.com>
2024-09-20 17:17:17 -0300

    .

---
+++ exercicios/ex007/index.js
@@ -0,0 +1,30 @@
+var c = 6
+
+while(c <= 6){
+    console.log(`faltam ${c} !`)
+    c++
+
+}
+
+///////////////////////////////////////
+
+var d = 1 
+
+do { 
+    console.log(`faltam ${d}`)
+    c++
+    
+}while(c <= 6)
+
+
+//////////////////////////////////
+
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+console.log(`tudo bem !`)
+
+////////////////////////////
