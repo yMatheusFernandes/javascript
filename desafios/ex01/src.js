717fb994cc9035c4999ff3bcf24442f0f45dad88
matheus <matheuz1398@gmail.com>
2024-09-24 16:19:53 -0300

    Update src.js

--- desafios/ex01/src.js
+++ desafios/ex01/src.js
@@ -6,13 +6,17 @@ function contar(){
     var resultado = document.getElementById(`resultado`)
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert(`Erro faltam dados!`)
+        resultado.innerHTML = `Impossivel contar!`
+        //window.alert(`Erro faltam dados!`)
     }else{
-        resultado.innerHTML = 'resultado '
-
+        resultado.innerHTML = 'resultado: <br>'
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0){
+            window.alert("passo invalido! Considerando passo 1")
+            p = 1
+        }
         if (i < f){
             //contagem crescente
             for(var c = i; c <=f ; c+=p){
@@ -21,7 +25,7 @@ function contar(){
         } else {
             //contagem regressiva
             for (var c = i ; c >= f; c-= p){
-                resultado.innerHTML += `\u{1F449}`
+                resultado.innerHTML += `${c} \u{1F449}`
             }
         }
         resultado.innerHTML += `\u{1F3C1}`
