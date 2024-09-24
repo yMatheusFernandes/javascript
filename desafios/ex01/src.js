addf4c765a19af20302adf366450bcd3ec21e2c7
matheus <matheuz1398@gmail.com>
2024-09-24 15:46:03 -0300

    Update src.js

--- desafios/ex01/src.js
+++ desafios/ex01/src.js
@@ -6,14 +6,24 @@ function contar(){
     var resultado = document.getElementById(`resultado`)
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert(`erro!`)
+        window.alert(`Erro faltam dados!`)
     }else{
-        resultado.innerHTML(`contando`)
-        var i = number(inicio.value)
-        var f = number(fim.value)
-        var p = number(passo.value)
+        resultado.innerHTML = 'resultado '
 
-        for(var c = 1; resultado <=fim ; c+passo)
-
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f){
+            //contagem crescente
+            for(var c = i; c <=f ; c+=p){
+                resultado.innerHTML +=`${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for (var c = i ; c >= f; c-= p){
+                resultado.innerHTML += `\u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
     }
 }
