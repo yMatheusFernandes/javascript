cb9edf04015f5e1925caed44b2b2b8033a00d720
matheus <matheuz1398@gmail.com>
2024-09-25 17:04:47 -0300

    .

---
+++ desafios/ex02/src.js
@@ -0,0 +1,10 @@
+function tabuada(){
+    var num = document.getElementById (`txtn`)
+    var tab = docuemnt.getElementById (`seltab`)
+    if (num.value.length == 0){
+        window.alert('Digite um valor, por favor!')
+    }else{
+        var n = Number(num.value)
+    }
+    
+}
