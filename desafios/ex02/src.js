9267d1a7a789adb5d1e50fc6f0f6e626cdaadffc
matheus <matheuz1398@gmail.com>
2024-09-26 15:31:05 -0300

    .

--- desafios/ex02/src.js
+++ desafios/ex02/src.js
@@ -1,10 +1,19 @@
 function tabuada(){
     var num = document.getElementById (`txtn`)
-    var tab = docuemnt.getElementById (`seltab`)
+    var tab = document.getElementById (`seltab`)
     if (num.value.length == 0){
         window.alert('Digite um valor, por favor!')
     }else{
         var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ``
+        while (c <= 10) {
+            var item = document.createElement(`option`)
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+
+        }
     }
-    
 }
