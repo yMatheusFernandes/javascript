4d771209d1030fd271f7b53f699acd84756c629e
matheus <matheuz1398@gmail.com>
2024-10-01 17:36:24 -0300

    .

--- desafios/ex03/scr01.js
+++ desafios/ex03/scr01.js
@@ -1,1 +1,29 @@
-var n1 = 2
+let num = document.querySelector(`input#fnum`)
+let lista = document.querySelector(`select#flista`)
+let res = document.querySelector(`div#res`)
+let valores = []
+
+function isNumero(n) {
+    if(Number (n) >=1 && Number(n) <=100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n,l){
+    if (l.indexOf(Number(n)) != -1){
+        return true 
+    }else{
+        return false
+    }
+}
+
+function adicionar (){
+    if(isNumero(num.value)&& !inlista(num.value, valores)){
+
+
+    }else{
+        window.alert(`valores invalidos ou ja encontrados na lista`)
+    }
+}
