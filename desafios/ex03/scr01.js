2e3e0a16c68e4dc3e4ed3563e12ebfc5ae9eab8d
matheus <matheuz1398@gmail.com>
2024-10-01 21:36:55 -0300

    finish

--- desafios/ex03/scr01.js
+++ desafios/ex03/scr01.js
@@ -21,9 +21,40 @@ function inlista(n,l){
 
 function adicionar (){
     if(isNumero(num.value)&& !inlista(num.value, valores)){
-
-
+        valores.push(Number(num.value))
+        let item = document.createElement(`option`)
+        item.text = `valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ``
     }else{
-        window.alert(`valores invalidos ou ja encontrados na lista`)
+        window.alert(`valore invalido ou ja encontrado na lista`)
+    }
+    num.value = ``
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert(`adicione valores antes de finalizar!`)
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > [maior])
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/tot
+        res.innerHTML = ``
+        res.innerHTML += `<p>Ao todo ,Temos ${tot} numeros cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores e ${media}</p>`
     }
 }
